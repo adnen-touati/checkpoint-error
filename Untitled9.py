95ed50ab9f4068508a7230be32da03e960b0ac78
adnen-touati <101455833+adnen-touati@users.noreply.github.com>
2022-06-05 10:29:08 +0100

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#quest1
+mylist=[14, "hello", 967]
+mylist[2]
+
+
+# In[4]:
+
+
+#quest2
+import pandas
+import numpy
+
+
+# In[6]:
+
+
+#quest 3
+print("python errors")
+
+
+# In[10]:
+
+
+#quest 4
+mydictionnary={"True":"hello","False":"bye", '3':"python"}
+mydictionnary['True']
+
+
+# In[11]:
+
+
+#quest 5
+i=14
+while i<78:
+    print(i)
+    i+=1
+
+
+# In[13]:
+
+
+#quest 6
+it=iter([1,2,3])
+next(it)
+next(it)
+next(it)
+
+
+# In[14]:
+
+
+#quest 7
+int('15')+15
+
+
+# In[ ]:
+
+
+#quest 8
+int('python')==> #la valeur doit etre numérique 
+
+
+# In[ ]:
+
+
+#quest 10
+x=19/0 # division par zéro est impossible
+
